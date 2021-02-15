3d24d2b211aea0ed31343ebc8263ecd497cf4bf8
scott95100 <scott95100@gmail.com>
2021-02-15 15:32:20 -0700

    building out the serch engine API and the views for search results

--- controllers/lumber.js
+++ controllers/lumber.js
@@ -1,3 +1,4 @@
+const { request } = require('express')
 let express = require('express')
 let db = require('../models')
 let router = express.Router()
@@ -6,5 +7,42 @@ let router = express.Router()
 router.get('/new', (req, res) => {
     res.render('lumber/new')
 })
+
+
+router.get('/results', (req, res)=>{
+    request({
+        url: 'https://trefle.io/',
+        qs: {
+            s: req.query.searchTerm
+        }
+    }, function(error, response, body) {
+        if(!error && response.statusCode === 200) {
+            const plantData = JSON.parse(body);
+            res.render('results.ejs', { Plant : plantData.Search });
+        }else {
+            res.send('Error');
+        }
+    })
+})
+
+
+router.get('/plant/:trefleId', (req, res)=> {
+    let trefleId = req.params.trefleId;
+    request({
+        url: 'https://trefle.io/',
+        qs: {
+            i: trefleId,
+            tomatoes: true
+        }
+    }, function (error, response, body) {
+        if(!error && response.statusCode ===200) {
+            const plantData = JSON.parse(body);
+            res.render('tree.ejs', { tree: plantData });
+        }else {
+            res.send('error');
+        }
+    })
+})
+
 
 module.exports = router
